33e846e253f79608b38e6a98d7d690fd21d47815
ishaan95 <ishaan.paranjape@gmail.com>
2018-11-25 23:02:45 -0800

    Part3_003

--- 1d_stencil_shared.cu
+++ 1d_stencil_shared.cu
@@ -10,13 +10,13 @@
 #include <stdio.h>
 #include <cuda_device_runtime_api.h>
 #include <chrono>
-#include <string>
+
 // define vector length, stencil radius,
 #define INPUTSIZE 64l
 #define N (1024*1024*INPUTSIZE)
 #define RADIUS 3
-#define GRIDSIZE 17//128
-#define BLOCKSIZE 160//256
+#define GRIDSIZE 128
+#define BLOCKSIZE 256
 
 int gridSize  = GRIDSIZE;
 int blockSize = BLOCKSIZE;
@@ -35,13 +35,15 @@ void cudaErrorCheck()
   std::cout << ">>>> " << s1 << std::endl;
 }
 
-void start_timer(cudaEvent_t* start) {
+void start_timer(cudaEvent_t* start)
+{
    // FIXME: ADD TIMING CODE, HERE, USE GLOBAL VARIABLES AS NEEDED.
    cudaEventCreate(start);
    cudaEventRecord(*start);
 }
 
-float stop_timer(cudaEvent_t* start, cudaEvent_t* stop) {
+float stop_timer(cudaEvent_t* start, cudaEvent_t* stop)
+{
    // FIXME: ADD TIMING CODE, HERE, USE GLOBAL VARIABLES AS NEEDED.
    cudaEventCreate(stop);
    cudaEventRecord(*stop);
@@ -53,20 +55,23 @@ float stop_timer(cudaEvent_t* start, cudaEvent_t* stop) {
 
 cudaDeviceProp prop;
 int device;
-void getDeviceProperties() {
-   // FIXME: Implement this function so as to acquire and print the following
-   // device properties:
-   //    Major and minor CUDA capability, total device global memory,
-   //    size of shared memory per block, number of registers per block,
-   //    warp size, max number of threads per block, number of multi-prccessors
-   //    (SMs) per device, Maximum number of threads per block dimension (x,y,z),
-   //    Maximumum number of blocks per grid dimension (x,y,z).
-   //
-   // These properties can be useful to dynamically optimize programs.  For
-   // instance the number of SMs can be useful as a heuristic to determine
-   // how many is a good number of blocks to use.  The total device global
-   // memory might be important to know just how much data to operate on at
-   // once.
+void getDeviceProperties()
+{
+   /*
+   FIXME: Implement this function so as to acquire and print the following
+   device properties:
+      Major and minor CUDA capability, total device global memory,
+      size of shared memory per block, number of registers per block,
+      warp size, max number of threads per block, number of multi-prccessors
+      (SMs) per device, Maximum number of threads per block dimension (x,y,z),
+      Maximumum number of blocks per grid dimension (x,y,z).
+
+   These properties can be useful to dynamically optimize programs.  For
+   instance the number of SMs can be useful as a heuristic to determine
+   how many is a good number of blocks to use.  The total device global
+   memory might be important to know just how much data to operate on at
+   once.
+   */
 
    cudaGetDevice(&device);
    cudaGetDeviceProperties(&prop, device);
@@ -85,68 +90,84 @@ void getDeviceProperties() {
 
 void newline() { std::cout << std::endl; };
 
-void printThreadSizes() {
+void printThreadSizes()
+{
    int noOfThreads = gridSize * blockSize;
    printf("Blocks            = %d\n", gridSize);  // no. of blocks to launch.
    printf("Threads per block = %d\n", blockSize); // no. of threads to launch.
    printf("Total threads     = %d\n", noOfThreads);
    printf("Number of grids   = %d\n", (N + noOfThreads -1)/ noOfThreads);
 }
-
-
-
-void printArray(int *in, int length){
-        for(int i = 0; i < length; i++){
-                std::cout << in[i] << " ";
-        }
-        std::cout << "---------------------------------------------------" << std::endl;
-}
-
-// -------------------------------------------------------
-// CUDA device function that performs 1D stencil operation
-// -------------------------------------------------------
-__global__ void stencil_1D(int *in, int *out, long dim, int blocksize){
-
+/*
+-------------------------------------------------------
+CUDA device function that performs 1D stencil operation
+-------------------------------------------------------
+*/
+__global__ void stencil_1D(int *in, int *out, long dim)
+{
   __shared__ int temp[BLOCKSIZE + 2*RADIUS];
 
   long gindex = threadIdx.x + blockDim.x * blockIdx.x;
-  int lindex = threadIdx.x + RADIUS;
   int stride = gridDim.x * blockDim.x;
   int tid = threadIdx.x;
+  int lindex = threadIdx.x + RADIUS;
 
   // Go through all data
   // Step all threads in a block to avoid synchronization problem
-  while ( gindex < (dim + blockDim.x) ) {
-
-    /* FIXME PART 2 - MODIFIY PROGRAM TO USE SHARED MEMORY. */
-    temp[lindex] = in[gindex];
-
-    if(tid < RADIUS){
-	if(gindex < RADIUS){temp[lindex - RADIUS] = 0;}
-	else{temp[lindex - RADIUS] = in[gindex - RADIUS];}
-	if(gindex + blocksize >= N){
-		int diff = gindex + blocksize - N;
-		if(diff < 3){temp[lindex + blocksize] = 0;}
-		else{temp[lindex + blocksize] = 0;}
-	}
-	else{temp[lindex + BLOCKSIZE] = in[gindex + blocksize];}
+  while ( gindex < dim + blockDim.x)
+  {/* FIXME PART 2 - MODIFY PROGRAM TO USE SHARED MEMORY. */
+    if (gindex < N)
+    {
+      temp[lindex] = in[gindex];
+      if(tid < RADIUS)
+      {
+        if(gindex < RADIUS)
+        {
+          temp[lindex - RADIUS] = 0;//the very first halo
+        }
+        else
+        {
+          temp[lindex - RADIUS] = in[gindex - RADIUS];//fill up the left halo
+        }
+	      if(gindex + blocksize >= N)//check if it's the last block
+        {
+		      temp[lindex + blocksize] = 0;//the very last halo
+        }
+		    else
+        {
+          temp[lindex + blocksize] = in[gindex + blocksize];//fill up the right halo
+        }
+	    }
+    }
+    else
+    {
+      temp[lindex] = 0;
+      temp[lindex + blocksize] = 0;
+      if (tid < RADIUS)
+      {
+        if(gindex < RADIUS)
+        {
+          temp[lindex - RADIUS] = 0;//the very first halo
+        }
+        else
+        {
+          temp[lindex - RADIUS] = in[gindex - RADIUS];//fill up the left halo
+        }
+      }
     }
 
-   __syncthreads();   
 
+   __syncthreads();
     // Apply the stencil
     int result = 0;
-    for (int offset = -RADIUS; offset <= RADIUS; offset++) {
-      if ( lindex + offset < dim && lindex + offset > -1)
-	        result += temp[lindex + offset];
+    for (int offset = -RADIUS; offset <= RADIUS; offset++)
+    {
+        if ( lindex + offset < dim && lindex + offset > -1)
+             result += temp[lindex + offset];
     }
 
     // Store the result
-    if (gindex < dim)
-      out[gindex] = result;
-
-    // Update global index and quit if we are done
-    gindex += stride;
+    out[gindex] = result;
 
     __syncthreads();
 
@@ -155,46 +176,59 @@ __global__ void stencil_1D(int *in, int *out, long dim, int blocksize){
 
 #define True  1
 #define False 0
-void checkResults(int *h_in, int *h_out, int DoCheck=True) {
-   // DO NOT CHANGE THIS CODE.
-   // CPU calculates the stencil from data in *h_in
-   // if DoCheck is True (default) it compares it with *h_out
-   // to check the operation of this code.
-   // If DoCheck is set to False, it can be used to time the CPU.
+void checkResults(int *h_in, int *h_out, int DoCheck=True)
+{  /*
+   DO NOT CHANGE THIS CODE.
+   CPU calculates the stencil from data in *h_in
+   if DoCheck is True (default) it compares it with *h_out
+   to check the operation of this code.
+   If DoCheck is set to False, it can be used to time the CPU.
+   */
    int i, j, ij, result, err;
    err = 0;
-   for (i=0; i<N; i++){  // major index.
+   for (i=0; i<N; i++)
+   {  // major index.
       result = 0;
-      for (j=-RADIUS; j<=RADIUS; j++){
+      for (j=-RADIUS; j<=RADIUS; j++)
+      {
          ij = i+j;
          if (ij>=0 && ij<N)
             result += h_in[ij];
       }
-      if (DoCheck) {  // print out some errors for debugging purposes.
-         if (h_out[i] != result) { // count errors.
+      if (DoCheck)
+      {  // print out some errors for debugging purposes.
+         if (h_out[i] != result)
+         { // count errors.
             err++;
-            if (err < 8) { // help debug
+            if (err < 8)
+            { // help debug
                printf("h_out[%d]=%d should be %d\n",i,h_out[i], result);
             };
          }
-      } else {  // for timing purposes.
+      }
+      else
+       {  // for timing purposes.
          h_out[i] = result;
       }
    }
 
-   if (DoCheck) { // report results.
-      if (err != 0){
+   if (DoCheck)
+   { // report results.
+      if (err != 0)
+      {
          printf("Error, %d elements do not match!\n", err);
-      } else {
+      }
+      else
+      {
          printf("Success! All elements match CPU result.\n");
       }
    }
 }
-
-
-// ------------
-// main program
-// ------------
+/*
+------------
+main program
+------------
+*/
 int main(void){
 
   int *h_in, *h_out;
@@ -237,16 +271,11 @@ int main(void){
   newline();
   printThreadSizes();
   start_timer(&start);
-  stencil_1D<<<gridSize,blockSize>>>(d_in, d_out, N, BLOCKSIZE);
+  stencil_1D<<<gridSize,blockSize>>>(d_in, d_out, N);
   std::cout << "Elapsed time: " << stop_timer(&start, &stop) << " ms" << std::endl;
   // copy results back to host
   cudaMemcpy(h_out, d_out, size, cudaMemcpyDeviceToHost);
   cudaErrorCheck();
-  //std::cout << "--------------------------------------------" << std::endl;
-  //printArray(h_in, N);
-  //printArray(h_out, N);
-  //std::cout << "--------------------------------------------" << std::endl;
-
   checkResults(h_in, h_out);
   //----------------------------------------------------------
 
@@ -266,13 +295,8 @@ int main(void){
   //=====================================================
 
   // deallocate host memory
-  std::cout << "--------------------------------------------" << std::endl;
-  //std::cout << std::string(h_in) << std::endl;
-  //std::cout << std::string(h_out) << std::endl;
-  //printArray(h_in, N);
-  //printArray(h_out, N);
   free(h_in);
   free(h_out);
-  std::cout << "N: " << N << std::endl;
+
   return 0;
 }
